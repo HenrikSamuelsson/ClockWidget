f788ec78235e446bb9027cc4027298df0fd2b1e6
Robson75 <rsamuelsson@gmail.com>
2014-06-12 17:04:04 +0200

    added string "test"

--- main/src/main/java/com/samdide/android/clockwidget/MainActivity.java
+++ main/src/main/java/com/samdide/android/clockwidget/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    private String test = "test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
