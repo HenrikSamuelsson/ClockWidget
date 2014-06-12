e9c43415e9d6cf21b942755b02359c68f88b2947
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-06-12 18:23:11 +0200

    The widget now displays a clock instead of the random data.

--- main/src/main/java/com/samdide/android/clockwidget/MyWidgetProvider.java
+++ main/src/main/java/com/samdide/android/clockwidget/MyWidgetProvider.java
@@ -1,48 +1,45 @@
 package com.samdide.android.clockwidget;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
 import android.widget.RemoteViews;
 
-import java.util.Random;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyWidgetProvider extends AppWidgetProvider {
-
-    private static final String ACTION_CLICK = "ACTION_CLICK";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
 
-        // Get all ids
-        ComponentName thisWidget = new ComponentName(context,
-                MyWidgetProvider.class);
-        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-        for (int widgetId : allWidgetIds) {
-            // create some random data
-            int number = (new Random().nextInt(100));
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1, 1000);
+    }
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.widget_layout);
-            Log.w("WidgetExample", String.valueOf(number));
-            // Set the text
-            remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+    private class MyTime extends TimerTask {
+        RemoteViews remoteViews;
+        AppWidgetManager appWidgetManager;
+        ComponentName thisWidget;
+        DateFormat format = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
 
-            // Register an onClickListener
-            Intent intent = new Intent(context, MyWidgetProvider.class);
+        public MyTime(Context context, AppWidgetManager appWidgetManager) {
+            this.appWidgetManager = appWidgetManager;
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            thisWidget = new ComponentName(context, MyWidgetProvider.class);
+        }
 
-            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
-            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        @Override
+        public void run() {
+            remoteViews.setTextViewText(R.id.clock, "TIME = " + format.format(new Date()
+            ));
+            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
         }
     }
 }
